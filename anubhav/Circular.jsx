4d80708ab75548287bc7d25d5680837ea848904e
AsukaAzure <anushkumar128@gmail.com>
2025-06-27 10:11:10 +0530

    Add product showcase, gallery carousel, and testimonial section components
    - Implemented ProductShowcase component to display various GeoMagic products with detailed descriptions and specifications.
    - Created SmoothCoverflowCarousel for a responsive image gallery with touch support and autoplay functionality.
    - Developed TeamSection for displaying testimonials from users, featuring a carousel effect for seamless navigation.

---
+++ anubhav/Circular.jsx
@@ -0,0 +1,10 @@
+import CircularGallery from './Gallery';
+
+
+export default function Image(){
+    return(
+        <div className="relative h-[350px] sm:h-[500px] md:h-[700px]">
+  <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
+</div>
+    )
+}
